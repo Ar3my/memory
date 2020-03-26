a689d4366bcb4b8e5b4b8cbfb56e114091a2b2a1
Antoine REMY <antoineremy@macbook-air-de-antoine.home>
2020-03-26 12:13:33 +0100

    remove i8ln for  first

--- app/plugins/i18n.js
+++ app/plugins/i18n.js
@@ -1,20 +1,20 @@
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
-
-const FR = require('~/lang/fr.json');
-const EN = require('~/lang/en.json');
-
-Vue.use(VueI18n);
-
-export default ({ app }) => {
-  // Set i18n instance on app
-  // This way we can use it in middleware and pages asyncData/fetch
-  app.i18n = new VueI18n({
-    locale: 'fr',
-    fallbackLocale: 'fr',
-    messages: {
-      en: EN,
-      fr: FR,
-    },
-  });
-};
+// import Vue from 'vue';
+// import VueI18n from 'vue-i18n';
+//
+// const FR = require('~/lang/fr.json');
+// const EN = require('~/lang/en.json');
+//
+// Vue.use(VueI18n);
+//
+// export default ({ app }) => {
+//   // Set i18n instance on app
+//   // This way we can use it in middleware and pages asyncData/fetch
+//   app.i18n = new VueI18n({
+//     locale: 'fr',
+//     fallbackLocale: 'fr',
+//     messages: {
+//       en: EN,
+//       fr: FR,
+//     },
+//   });
+// };
